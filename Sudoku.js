746fee06907bbe27c079ae9b2027b5ed0238d53b
AFuentes1 <anthonyfuentescalvo@gmail.com>
2023-03-29 22:34:00 -0600

    [ADD] se agrega el tablero por cuadrado

--- Sudoku.js
+++ Sudoku.js
@@ -1,13 +1,30 @@
 const tableroSudoku = document.querySelector('#sudoku')
-const cuadrados = 441
+const cuadrados = 9
 
-for (let i = 0; i < cuadrados; i++) { //se crea un cuadrado del total del sudoku
-    const inputElement = document.createElement('input') //Se crea un input por cada cuadrado donde se recibira el numero
-    inputElement.setAttribute('type', 'number')
-    inputElement.setAttribute('min', 0) //El numero minimo y maximo para la entrada a cada cuadrado
-    inputElement.setAttribute('max', 9)
+function cuadrado () {
+    for (let i = 0; i < cuadrados; i++) {
+        const inputElement = document.createElement('input')
+        inputElement.setAttribute('type', 'number')
+        inputElement.setAttribute('min', 0)
+        inputElement.setAttribute('max', 9)
+        tableroSudoku.appendChild(inputElement)
+        
+    }
+}
 
-    /*if (
+function crearTablero(){
+
+    for (let i = 0; i < 7; i++) {
+        
+        cuadrado();  
+        
+    }
+}
+
+crearTablero();
+
+    /*
+    if (
         ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21)||   //Se agregan los bordes del tablero
         ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
         ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
@@ -16,13 +33,6 @@ for (let i = 0; i < cuadrados; i++) { //se crea un cuadrado del total del sudoku
     ){
         inputElement.classList.add('odd-section')
         
-    }
-    */
-    
+    }*/
+    //Se agrega el input al tablero
 
-
-
-
-    tableroSudoku.appendChild(inputElement) //Se agrega el input al tablero
-
-}
